bfc8f06ce68b31d390a400e8a751ea25142b9e2f
ddt-jissyusei1 <ddt.jissyusei1@gmail.com>
2017-04-21 00:33:39 +0900

    演習4-6 100%

--- src/kensyu01/E4_6_PutAsterisk2.java
+++ src/kensyu01/E4_6_PutAsterisk2.java
@@ -1,13 +1,13 @@
 package kensyu01;
 
-//入力を扱う機能の読み込み
+//入力を扱う機能の読み込みを行う
 import java.util.Scanner;
 
 
 //入力した数の文だけアスタリスクを出力するクラス
 public class E4_6_PutAsterisk2 {
 
-	// 入力した数分だけアスタリスクを出力する処理
+	// 入力した数分だけアスタリスクを出力する処理を行う
 	public static void main (String[] arg) {
 		
 		// 入力された値を保持する変数を0で初期化する
@@ -17,13 +17,13 @@ public class E4_6_PutAsterisk2 {
 		int loopCount = 1;
 		
 
-		// 入力した値を扱うオブジェクトの作成
+		// 入力した値を扱うオブジェクトの作成する
 		Scanner standardInput = new Scanner(System.in);
 		
 		// アスタリスクの個数入力を促すメッセージを出力する
 		System.out.println("何個*を表示しますか？ : ");
 		
-		// 入力した値が整数値出ない場合の処理を行う
+		// 入力した値が整数値でない場合の処理を行う
 		while ( !standardInput.hasNextInt() ) {
 			
 			// 入力が間違っていることをメッセージで出力する
@@ -40,7 +40,7 @@ public class E4_6_PutAsterisk2 {
 		// 入力された値を変数に保持する
 		inputValue = standardInput.nextInt();
 		
-		// 入力された値が1未満だった場合の処理
+		// 入力された値が1未満だった場合の処理を行う
 		if ( inputValue <= 0 ) {
 			
 			// 入力ストリームを閉じる
